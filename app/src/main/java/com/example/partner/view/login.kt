48aaca29eecc0e3211719db347931dff2ea92c35
mvhf2001 <mvhf2001@hotmail.com>
2024-05-20 13:57:54 -0300

    FEATURE - telas home,login e cadastro

---
+++ app/src/main/java/com/example/partner/view/login.kt
@@ -0,0 +1,22 @@
+package com.example.partner.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hello_world.R
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide() // Adicionado para remover a barra de título
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
